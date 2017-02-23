fa926bbc8e375a9949409728d718b879d917288d
Surya Setiyaputra <hello@setiyaputra.me>
2017-02-23 22:10:27 +1100

    Removed unnecessary lines

--- food2fork.rb
+++ food2fork.rb
@@ -6,10 +6,7 @@ require 'rest-client'
 require 'food2fork'
 require 'html_to_plain_text'
 
-# class FindRecipe
-#   def initialize(credentials)
-#     @credentials = credentials
-#   end
+FOOD2FORK_API_KEY = get_food2fork_credentials
 
   def get_recipe(ingredients)
     found = Food2Fork::Recipe.search({q: "#{ingredients}", sort: 'r', page: 1})# => returns list of Recipe array object
@@ -22,25 +19,3 @@ require 'html_to_plain_text'
     # Santize the HTML documents
     text = HtmlToPlainText.plain_text(page.to_s)
   end
-
-
-
-# end
-
-FOOD2FORK_API_KEY = get_food2fork_credentials
-
-
-# FOOD2FORK_API_KEY = get_food2fork_credentials
-# found = Food2Fork::Recipe.search({q: "#{user_1.ingredients}", sort: 'r', page: 1})# => returns list of Recipe array object
-
-# Fetch and parse HTML document
-# page = Nokogiri::HTML(RestClient.get(found.pop.f2f_url))
-
-# Sanitize HTML tag and print document
-# text = HtmlToPlainText.plain_text(page.to_s)
-# puts text
-
-#p html.xpath('//*[preceding-sibling::h5[1]][following-sibling::h5[2]]').map(&:content)
-
-# Print out extracted text (each element's inner text is separated by newlines)
-#puts extracted_text.join("\n")
