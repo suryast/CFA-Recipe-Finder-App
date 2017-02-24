949ae4ea412502f46a36325a95546483e517dc74
Ethan Huynh <Ethan@E-and-R-MacBook-Pro.local>
2017-02-24 11:21:16 +1100

    updating test class

--- test_takeaway.rb
+++ test_takeaway.rb
@@ -6,7 +6,7 @@ class TakeAwayTest < Test::Unit::TestCase
 
   def test_location
   #test case
-  customer = TakeAway.new("2010", "Dairy", "Italian", "30")
+  customer = PlacesSearch.new("client", "2010", "Italian")
   #assertion
   expected = "2010"
   actual = customer.location
@@ -17,7 +17,7 @@ class TakeAwayTest < Test::Unit::TestCase
 
   def test_location_non_postcode
   #test case
-  customer = TakeAway.new("earth", "Dairy", "Italian", "30")
+  customer = PlacesSearch.new("client","earth", "Italian")
   #assertion
   expected = "nope"
   actual = customer.location
@@ -28,7 +28,7 @@ class TakeAwayTest < Test::Unit::TestCase
 
   def test_cuisine
   #test case
-  customer = TakeAway.new("2010", "Dairy", "Italian", "30")
+  customer = PlacesSearch.new("client", "2010", "Italian")
   #assertion
   expected = "Italian"
   actual = customer.cuisine
@@ -37,13 +37,5 @@ class TakeAwayTest < Test::Unit::TestCase
   assert_equal(expected, actual, msg)
   end
 
-  def test_takeaway_suggestion
-  #testing customer with no allergies
-  customer = TakeAway.new("2060", "", "Italian", "30")
-  expected = ""
-  actual = customer.allergen
-  msg = "#{customer.location}, #{customer.allergen}, #{customer.cuisine}, #{customer.suitable_age_group}"
-  assert_equal(expected, actual, msg)
-  end
 
 end
